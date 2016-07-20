8396ac27f0527d2196c7055661885f387520fe7d
Mario John Villaplana <mario.villaplana@gmail.com>
2016-07-20 12:30:33 -0600

    separate dirs and add a haskell thing

---
+++ haskell/1.hs
@@ -0,0 +1,6 @@
+doubleMe x = x + x
+doubleSmallNumber x = if x > 100
+                            then x
+                            else x *2
+
+doubleSmallNumberPlusOne x = (if x > 100 then x else x*2) + 1
